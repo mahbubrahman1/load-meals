eba85f32736c5e28d6e66e31c8ffb74197448800
mahbubrahman1 <mahbuburrofficial@gmail.com>
2022-03-29 19:06:18 +0600

    added funtionality

--- js/script.js
+++ js/script.js
@@ -5,5 +5,23 @@ const searchFood = () => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data))
-}
+        .then(data => displaySearchResult(data.meals))
+};
+
+const displaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    meals.forEach(meal => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+            <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        `;
+        searchResult.appendChild(div);
+    });
+};
